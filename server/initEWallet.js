1d51e658a19ac1902cbe99b0d99ead18306c6c8d
Art Nattapat <nattapat.art@gmail.com>
2017-06-23 19:26:14 +0700

    feat(EWallet): can connect with eWallet service

--- server/initEWallet.js
+++ server/initEWallet.js
@@ -7,20 +7,51 @@ const wallet = {
 }
 
 const init = (config, app) => {
-  global.NAP.EWallet = {
-    getEWallet: token => ({
-      hasReceipt: async ({ refId, spendType }) => !!wallet.receipts.find(r => r === refId),
-      getCoin: async () => ({ gold: wallet.gold, silver: wallet.silver }),
-      spendCoin: async ({ refId, spendType, merchantId, merchantAlaisId, amount, currencyType, commissionRate }) => {
-        if (currencyType === 'gold') {
-          wallet.gold -= amount
-        } else {
-          wallet.silver -= amount
+  const api = config.e_wallet_api
+  if (api === 'DEV') {
+    global.NAP.EWallet = {
+      getEWallet: token => ({
+        hasReceipt: async ({ refId, spendType }) => !!wallet.receipts.find(r => r === refId),
+        getJelly: async () => ({ gold: wallet.gold, silver: wallet.silver }),
+        spendJelly: async ({ refId, spendType, merchantId, merchantAlaisId, amount, currencyType, commissionRate }) => {
+          if (currencyType === 'gold') {
+            wallet.gold -= amount
+          } else {
+            wallet.silver -= amount
+          }
+          wallet.receipts.push(refId)
+          return { gold: wallet.gold, silver: wallet.silver }
         }
-        wallet.receipts.push(refId)
-        return { gold: wallet.gold, silver: wallet.silver }
+      })
+    }
+  } else {
+    global.NAP.EWallet = {
+      getEWallet: token => {
+        const callApi = async (path, data = {}) => {
+          const result = await request
+            .post(`${api}/v1/${path}`)
+            .set('Content-Type', 'application/json')
+            .send(Object.assign({
+              token
+            }, data))
+          return result.body[0]
+        }
+        return {
+          hasReceipt: async ({ refId, spendType }) => {
+            const result = await callApi('spend/hasReceipt', { refId, spendType })
+            return result.status
+          },
+          getJelly: async () => {
+            const result = await callApi('user/getJelly')
+            return result.reader
+          },
+          spendJelly: async ({ refId, spendType, merchantId, merchantAlaisId, amount, currencyType, commissionRate }) => {
+            const result = await callApi('spend/spendJelly', { refId, spendType, merchantId, merchantAlaisId, amount, currencyType, commissionRate })
+            return result.reader
+          }
+        }
       }
-    })
+    }
   }
 }
 
