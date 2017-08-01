cddddb239d4baf244e993b422e0a98740c2ee676
Rungsikorn Rungsikavanich <rungsikorn@me.com>
2017-08-01 20:43:12 +0700

    feat(add author seed)

---
+++ templates/__test__/email-forget.spec.js
@@ -0,0 +1,15 @@
+const renderEmail = require('../email-forget')
+const fs = require('fs')
+const path = require('path')
+
+describe('Email template test', () => {
+  it('should render html string', () => {
+    const htmlString = renderEmail(
+      'foo@bar.com',
+      'bar@foo.bar',
+      'foo.bar.com',
+      'bar@foo.com/?id=bababababab'
+    )
+    fs.writeFileSync(path.join(__dirname, 'email.tmp.html'), htmlString.text)
+  })
+})
--- templates/email-forget.js
+++ templates/email-forget.js
@@ -1,17 +1,55 @@
 // User will receive this email to reset password
 // @see https://github.com/mailgun/mailgun-js#messages
+
+const contentStyle = `
+  background-color: #f9fafb;
+  padding: 40px 40px;
+  border: 1px solid #ebebeb;
+  text-align: center;
+`
+const footerContainerStyle = `
+  margin-top: 20px;
+  margin-bottom: 20px;
+  text-align: center;
+  color: #666;
+`
+const footerItemWrap = `
+  display: inline-block;
+`
+const footerItemBorder = `
+  border-right: 1px solid #ebebeb;
+  padding-right: 10px;
+  margin-right: 10px;
+`
+const contactStyle = `
+  font-size: 22px;
+  color: #333;
+`
 module.exports = (domain, email, password_reset_url, new_password_reset_url) => ({
   from: 'noreply@' + domain,
   to: [email],
   subject: `[${domain}] Please reset your password`,
-  text: `We heard that you lost your ${domain} password. Sorry about that!
+  text: `
+  
+  <img src='http://static.jamplay.world:3002/static/logo-orange@2x.png' style='width:150px;margin-bottom:10px' /> 
+  <div style='font-size:26px;font-weight:bold;color:white;background-color:#df6336;padding: 20px;text-align:center;'>เปลี่ยนรหัสผ่านใหม่</div> 
+  <div style='${contentStyle}'>
+    <p>
+      กดลิ้งค์ด้านล่าง เพื่อดำเนินการเปลี่ยนรหัสผ่านใหม่
+    </p>
+    <p>
+      <a href='${password_reset_url}'>${password_reset_url}</a>
+    </p>
+  
+  </div>
+  <div style='${footerContainerStyle}'>
+    <a style='${footerItemWrap}${footerItemBorder}' href='http://www.jamplay.world/about'>เกี่ยวกับ JamPlay</a>
+    <a style='${footerItemWrap}${footerItemBorder}' href='http://www.jamplay.world/privacy'>นโยบาลความเป็นส่วนตัว</a>
+    <a style='${footerItemWrap}' href='http://www.jamplay.world/agreement'>ข้อตกลงการใช้บริการ</a>
+  </div>
 
-But don’t worry! You can use the following link within the next day to reset your password:
-
-${password_reset_url}
-
-If you don’t use this link within 24 hours, it will expire.
-To get a new password reset link, visit ${new_password_reset_url}
-
-Thanks,`
+  <div style='${footerContainerStyle}'>
+    ติดต่อเรา <span style='${contactStyle}'> โทร. 02-2580486 ต่อ 115 </span> หรือ <span style='${contactStyle}'> อีเมล. artery@keystream.co </span>
+  </div>
+  `
 })
