79217cea0dd2f2cb6be217d58c04ce9b9275fe0d
mikeccuk2005 <magic.code.interactive@gmail.com>
2017-10-24 11:13:16 +0700

    chore(TWITTER): Add twitter api error response
    Closes #JAMP-289

--- server/initTwitter.js
+++ server/initTwitter.js
@@ -1,10 +1,16 @@
 const { getOAuthRequestToken, getAccessToken } = require('../sharer/twitterHelper')
 const Twitter = require('twitter')
+const chalk = require('chalk')
 
 // Init
 const init = ({ twitter_consumer_key, twitter_consumer_secret }, app) => {
   const consumerKey = twitter_consumer_key
   const consumerSecret = twitter_consumer_secret
+
+  if (!consumerKey || !consumerSecret) {
+    console.warn(chalk.bgYellow('TWITTER consumerSecret not found! Therefore TWITTER feature will not work'))
+    return
+  }
 
   // EXPRESS
   app.get('/twitter/request_token', async (req, res) => {
@@ -16,6 +22,7 @@ const init = ({ twitter_consumer_key, twitter_consumer_secret }, app) => {
       res.send(JSON.stringify(result)) // send back url to login twitter
     } catch (error) {
       console.log('/twitter/request_token error', { error })
+      res.status(500).send(error.toString())
     }
   })
 
@@ -30,33 +37,38 @@ const init = ({ twitter_consumer_key, twitter_consumer_secret }, app) => {
       res.send(JSON.stringify(result))
     } catch (error) {
       console.log('/twitter/access_token error', { error })
+      res.status(500).send(error.toString())
     }
   })
 
   app.post('/twitter/post', (req, res) => {
-    const { accessToken, status } = req.body
-    console.log('/twitter/post ', req.body)
-    const tokenSplit = accessToken.split('_')
-    const oauth_access_token = tokenSplit[0]
-    const oauth_access_token_secret = tokenSplit[1]
+    try {
+      const { accessToken, status } = req.body
+      console.log('/twitter/post ', req.body)
+      const tokenSplit = accessToken.split('_')
+      const oauth_access_token = tokenSplit[0]
+      const oauth_access_token_secret = tokenSplit[1]
 
-    const client = new Twitter({
-      consumer_key: consumerKey,
-      consumer_secret: consumerSecret,
-      access_token_key: oauth_access_token,
-      access_token_secret: oauth_access_token_secret
-    })
+      const client = new Twitter({
+        consumer_key: consumerKey,
+        consumer_secret: consumerSecret,
+        access_token_key: oauth_access_token,
+        access_token_secret: oauth_access_token_secret
+      })
 
-    client.post('statuses/update', { status }, (error, tweet, response) => {
-      if (error) {
-        console.log({ error })
-        return res.send(JSON.stringify({ error }))
-      }
+      client.post('statuses/update', { status }, (error, tweet, response) => {
+        if (error) {
+          console.log({ error })
+          return res.status(500).send(JSON.stringify({ error }))
+        }
 
-      res.send(JSON.stringify({ tweet }))
-      // console.log(tweet);  // Tweet body.
-      // console.log(response);  // Raw response object.
-    })
+        res.send(JSON.stringify({ tweet }))
+        // console.log(tweet);  // Tweet body.
+        // console.log(response);  // Raw response object.
+      })
+    } catch (error) {
+      res.status(500).send(error.toString())
+    }
   })
 }
 module.exports = init
