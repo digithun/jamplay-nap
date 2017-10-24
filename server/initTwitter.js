0298778abc2138781990974edb24e8819e5b3a8b
mikeccuk2005 <magic.code.interactive@gmail.com>
2017-10-24 17:37:05 +0700

    fix(TWITTER): Add error response

--- server/initTwitter.js
+++ server/initTwitter.js
@@ -13,13 +13,16 @@ const init = ({ twitter_consumer_key, twitter_consumer_secret }, app) => {
   }
 
   // EXPRESS
-  app.get('/twitter/request_token', async (req, res) => {
+  app.get('/twitter/request_token', (req, res) => {
     try {
       let { oauth_callback } = req.query
       oauth_callback = Buffer.from(oauth_callback, 'base64').toString('utf8')
-      console.log(oauth_callback)
-      let result = await getOAuthRequestToken({ consumerKey, consumerSecret, oauth_callback })
-      res.send(JSON.stringify(result)) // send back url to login twitter
+      getOAuthRequestToken({ consumerKey, consumerSecret, oauth_callback })
+        .then(result => res.send(JSON.stringify(result)))
+        .catch(error => {
+          console.log('/twitter/request_token error/getOAuthRequestToken', { error })
+          res.status(500).send(error.toString())
+        })
     } catch (error) {
       console.log('/twitter/request_token error', { error })
       res.status(500).send(error.toString())
@@ -27,14 +30,15 @@ const init = ({ twitter_consumer_key, twitter_consumer_secret }, app) => {
   })
 
   // TWITTER hook callback
-  app.get('/twitter/access_token', async (req, res) => {
+  app.get('/twitter/access_token', (req, res) => {
     try {
-      console.log('/twitter/access_token query', req.query)
-
       const { oauth_token, oauth_verifier, oauth_token_secret } = req.query
-
-      let result = await getAccessToken({ oauth_token, oauth_token_secret, oauth_verifier, consumerKey, consumerSecret })
-      res.send(JSON.stringify(result))
+      let result = getAccessToken({ oauth_token, oauth_token_secret, oauth_verifier, consumerKey, consumerSecret })
+        .then(result => res.send(JSON.stringify(result)))
+        .catch(error => {
+          console.log('/twitter/access_token error/getAccessToken', { error })
+          res.status(500).send(error.toString())
+        })
     } catch (error) {
       console.log('/twitter/access_token error', { error })
       res.status(500).send(error.toString())
@@ -44,7 +48,6 @@ const init = ({ twitter_consumer_key, twitter_consumer_secret }, app) => {
   app.post('/twitter/post', (req, res) => {
     try {
       const { accessToken, status } = req.body
-      console.log('/twitter/post ', req.body)
       const tokenSplit = accessToken.split('_')
       const oauth_access_token = tokenSplit[0]
       const oauth_access_token_secret = tokenSplit[1]
@@ -61,7 +64,6 @@ const init = ({ twitter_consumer_key, twitter_consumer_secret }, app) => {
           console.log({ error })
           return res.status(500).send(JSON.stringify({ error }))
         }
-
         res.send(JSON.stringify({ tweet }))
         // console.log(tweet);  // Tweet body.
         // console.log(response);  // Raw response object.
