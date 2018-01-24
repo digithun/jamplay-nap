ca3ac8c91300efd6254d08b638d059f239e1577d
lasd <nattapat.art@gmail.com>
2018-01-24 22:37:57 +0700

    update scripts

--- scripts/build-image.bash
+++ scripts/build-image.bash
@@ -5,12 +5,13 @@ PACKAGE_VERSION=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
-echo 'Start build image with project id: ' $(gcloud config get-value project), tag: $PACKAGE_VERSION
 
 echo 'Branch' $BRANCH
 if [ "$BRANCH" == "master" ]; then
+echo 'Start build image with project id: ' $(gcloud config get-value project), tag: $PACKAGE_VERSION
 docker build -t gcr.io/$(gcloud config get-value project)/jamplay-nap:$PACKAGE_VERSION --rm -f jamplay.Dockerfile .
 docker tag gcr.io/$(gcloud config get-value project)/jamplay-nap:$PACKAGE_VERSION gcr.io/$(gcloud config get-value project)/jamplay-nap:latest
 else
+echo 'Start build image with project id: ' $(gcloud config get-value project), tag: latest
 docker build -t gcr.io/$(gcloud config get-value project)/jamplay-nap:latest --rm -f jamplay.Dockerfile .
 fi
--- scripts/push-gcloud.bash
+++ scripts/push-gcloud.bash
@@ -5,9 +5,10 @@ PACKAGE_VERSION=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
-echo 'Start push image with project id: ' $(gcloud config get-value project), tag: $PACKAGE_VERSION
 
+echo 'Start push image with project id: ' $(gcloud config get-value project), tag: latest
 gcloud docker -- push gcr.io/$(gcloud config get-value project)/jamplay-nap:latest
 if [ "$BRANCH" == "master" ]; then
+echo 'Start push image with project id: ' $(gcloud config get-value project), tag: $PACKAGE_VERSION
 gcloud docker -- push gcr.io/$(gcloud config get-value project)/jamplay-nap:$PACKAGE_VERSION
 fi
