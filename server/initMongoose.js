1ed9568fb66b2a3b65f992cc690fb126d923abb2
lasd <nattapat.art@gmail.com>
2018-02-23 17:06:20 +0700

    fix(initMongoose): setTimeout to exit if database not ready

--- server/initMongoose.js
+++ server/initMongoose.js
@@ -1,15 +1,30 @@
+const mongoose = require('mongoose')
+function checkReadyState () {
+  setTimeout(() => {
+    if (!mongoose.connection.readyState) {
+      console.error('database not ready')
+      process.exit(0)
+    }
+  }, 10000)
+}
+
 const init = async mongo_url => {
   debug.info(`MongoDB : ${mongo_url}`)
 
-  const mongoose = require('mongoose')
   mongoose.Promise = global.Promise
 
   // Debug
   mongoose.connection.on('connected', () => debug.info(`MongoDB :`, 'Connection Established'))
   mongoose.connection.on('reconnected', () => debug.info(`MongoDB :`, 'Connection Reestablished'))
-  mongoose.connection.on('disconnected', () => debug.info(`MongoDB :`, 'Connection Disconnected'))
+  mongoose.connection.on('disconnected', () => {
+    debug.info(`MongoDB :`, 'Connection Disconnected')
+    checkReadyState()
+  })
   mongoose.connection.on('close', () => debug.info(`MongoDB :`, 'Connection Closed'))
-  mongoose.connection.on('error', err => debug.error(`MongoDB :`, err))
+  mongoose.connection.on('error', err => {
+    debug.error(`MongoDB :`, err)
+    checkReadyState()
+  })
 
   return mongoose.connect(mongo_url, {
     useMongoClient: true,
